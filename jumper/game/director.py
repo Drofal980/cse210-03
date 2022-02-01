e62394095a2b98c7526e379fb5ebe72802bc8c3f
Brandon Van Blarcum <41463636+Drofal980@users.noreply.github.com>
2022-02-01 15:48:13 -0700

    Created foundational classes and designing files

---
+++ jumper/game/director.py
@@ -0,0 +1,51 @@
+
+class Director:
+    """A person who directs the game. 
+    
+    The responsibility of a Director is to control the sequence of play.
+
+    Attributes:
+        is_playing (boolean): Whether or not the game is being played.
+    """
+
+    def __init__(self):
+        """Constructs a new Director.
+        
+        Args:
+            self (Director): an instance of Director.
+        """
+
+    def start_game(self):
+        """Starts the game by running the main game loop.
+        
+        Args:
+            self (Director): an instance of Director.
+        """
+        while self.is_playing:
+            self.get_inputs()
+            self.do_updates()
+            self.do_outputs()
+
+    def get_inputs(self):
+        """Ask the user if they want to roll.
+
+        Args:
+            self (Director): An instance of Director.
+        """
+
+       
+    def do_updates(self):
+        """Updates the player's score.
+
+        Args:
+            self (Director): An instance of Director.
+        """
+        
+
+    def do_outputs(self):
+        """Displays the dice and the score. Also asks the player if they want to roll again. 
+
+        Args:
+            self (Director): An instance of Director.
+        """
+        
