ca1105a65cfd5e3c11730273d18fb4ff945e40ba
James R <Reedcj798@gmail.com>
2022-02-02 10:02:56 -0700

    Word_bank class upload

---
+++ jumper/game/word_bank.py
@@ -0,0 +1,13 @@
+from random import random
+
+class Word_bank:
+
+    def __init__(self):
+        self._choosen_word = ''
+        self._words = ['waffles','dogs','cats','class','happy']
+
+    def choose_word(self):
+        self._choosen_word = random(self.words)
+    
+    def get_word(self):
+        return self._choosen_word
