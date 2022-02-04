d7ba1474fd42c1ca96b231dff63c43ab3787679c
sobowaleo <86094324+sobowaleo@users.noreply.github.com>
2022-02-03 18:51:26 -0700

    Update jumper.py

--- jumper/game/jumper.py
+++ jumper/game/jumper.py
@@ -1,1 +1,14 @@
+class jumper:
+  
+  def __init__(self):
+    
+    picture = ["string1"
+      " ____",
+      "/____ \",
+      "\     /",
+       "\   /",
+         "o",
+        "/|\",
+        "/ \"
+    ]
 
