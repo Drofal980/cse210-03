53553ee2a9441a1dc49dff6d553755d29ce43870
Brandon Van Blarcum <41463636+Drofal980@users.noreply.github.com>
2022-03-30 09:28:32 -0600

    Revert "Encapsulated Jumper() and updated Director() for private vars"
    This reverts commit f0a6660c84cf5e2ff630cfa2e4f98956d835cee0.

--- jumper/game/jumper.py
+++ jumper/game/jumper.py
@@ -1,34 +1,34 @@
-class Jumper():
-    def __init__(self):
-        self._picture = ["",
-        "   ___",
-        "  /___\\",
-        "  \   /",
-        "   \ /",
-        "    0",
-        "   /|\\",
-        "   / \\"
-        ]
-        # maybe we could add a variable and a function to 
-        # return if jumper is dead?
+class jumper:
+  
+  def __init__(self):
+    #why string1 for first slot?
+    self._picture = ["",
+      " ___",
+      "/___\\",
+      "\   /",
+      " \ /",
+      "  0",
+      " /|\\",
+      " / \\"
+    ]
+  
+  def __str__(self):
+    for line in self._picture:
+      print(line)
+    return ""
+  
+  def __repr__(self):
+    self.__str__(self)
+  
+  def remove_line(self, index):
+    self._picture.pop(1)
 
-    def jumper_man(self):
-        for line in self._picture:
-            print(line)
-
-    def remove_jumper(self):
-        #check whats left of the picture or if dead
-            #self._picture = ["",
-            # "    x",
-            # "   /|\\",
-            # "   / \\"
-            # ]
-        #else
-        self._picture.pop(1)
-
-# For testing purposes, remove #
-# jumper = Jumper()
-# jumper.jumper_man()
-
-# jumper.remove_jumper()
-# jumper.jumper_man()
+  def if_dead(self):
+    if len(self._picture) <= 4:
+      self._picture = ["",
+      "  x",
+      " /|\\",
+      " / \\"
+    ]
+      return True
+    return False
